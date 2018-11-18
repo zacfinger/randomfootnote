dfe93d18ae651f38fc96fb133e8f54089c8b8b9f
Zac Finger <zac@Zacs-MacBook-Pro.local>
2018-11-18 15:24:56 -0700

    Cleaned up a bunch of stuff and removed encapsulation for randomly generating terms from arrays, not sure if best practice

--- main.js
+++ main.js
@@ -1,32 +1,55 @@
-/*//////////////
+/*/////////////////////////////////////////////////////////////////////////////////////////////
+///																							///
+///  Postmodern Article Citation Generator													///
+///  JavaScript port of Postmodernism Generator by Andrew C. Bulhak, Monash University		///
+///  // // // http://www.elsewhere.org/journal/pomo/										///
+///  // // // https://github.com/orenmazor/Dada-Engine/blob/master/scripts/pomo.pb			///
+///  // // // pomo.pb  acb  ??-09-24 AU 													///
+///  // // // pb script for generating postmodern verbiage									///
+///  // // // Updated, format-independent version											///
+///  // // // Copyright (C) 1995, 1996 Andrew C. Bulhak										///
+///  // // // this script is property of acb. You are permitted to use, modify and 			///
+///  // // // distribute it as long as this notice is retained and any modifications		///
+///  // // // in distributed copies are clearly denoted.									///
+///  Derivative port (GPL-3.0) #NanoGenMo 2018 ZacFinger.com								///
+///  https://github.com/zacfinger/randomfootnote/   										///
+///  https://twitter.com/randomfootnote             										///
+///																							///
+/////////////////////////////////////////////////////////////////////////////////////////////*/
 
-Postmodern Article Citation Generator
-// // // JavaScript port of Andrew C. Bulhak's
-// // // pb script for generating postmodern verbiage
-(GPL-3.0) 2018 ZacFinger.com.
-https://github.com/zacfinger/randomfootnote/
-https://twitter.com/randomfootnote
+/* TODO:
+// Refactor all code to use randomArrayIndex rather than encapsulated methods for the arrays
+// Fix 'candid title' so that all words are capitalized
+// // i.e. 'The discourse of paradigm' becomes "The Discourse of Paradigm"
+// // i.e. 'The burning house: Nihilism in the works of Glass'
+// Figure out why sometimes the titles have repetitive terms
+// // i.e., The reality of futility: Batailleist 'powerful communication', 
+// // cultural narrative and Batailleist 'powerful communication'
+// // // may want to return to randomArtMovement() type methods and define arrays locally
+// Retweet/reply to people using tag "citationneeded"
+// More topics and disciplines added
+// Use regex to teach yourself regular expressions
+// Fix pluralise errors
+// // i.e., 'Concensues of genre: The neocultural paradigm of discourse in the works of Pynchon'
 
-// pomo.pb  acb  ??-09-24 AU
-// pb script for generating postmodern verbiage
-// Updated, format-independent version
-// Copyright (C) 1995, 1996 Andrew C. Bulhak
-// this script is property of acb. You are permitted to use, modify and
-// distribute it as long as this notice is retained and any modifications
-// in distributed copies are clearly denoted.
-
-Based in whole or in part on:
+Works Cited:
 
 (1) https://github.com/orenmazor/Dada-Engine/blob/master/scripts/pomo.pb
-(2) LastName, F. (2015). Interplay: neo-geo neoconceptual art of the 1980s. Choice Reviews Online, 52(09), pp.52-4572-52-4572.
-(3) Adam, E. (2017). Intersectional Coalitions: The Paradoxes of Rights-Based Movement Building in LGBTQ and Immigrant Communities. Law & Society Review, 51(1), pp.132-167.
+(2) LastName, F. (2015). Interplay: neo-geo neoconceptual art of the 1980s. 
+				Choice Reviews Online, 52(09), pp.52-4572-52-4572.
+(3) Adam, E. (2017). Intersectional Coalitions: The Paradoxes of Rights-Based Movement Building 
+				in LGBTQ and Immigrant Communities. Law & Society Review, 51(1), pp.132-167.
 (4) lastname, f. (year). article title. journal name, randNum(randNum), pp.randNum-randNum.
-(5) Muspratt, M. and Steeves, H. (2012). Rejecting Erasure Tropes of Africa: The Amazing Race, Episodes in Ghana Counter Postcolonial Critiques. Communication, Culture & Critique, 5(4), pp.533-540.
+(5) Muspratt, M. and Steeves, H. (2012). Rejecting Erasure Tropes of Africa: The Amazing Race, 
+				Episodes in Ghana Counter Postcolonial Critiques. Communication, Culture & 
+				Critique, 5(4), pp.533-540.
+(6) http://irsc.libguides.com/mla/workscitedlist
 
-*/
+/////////////////////////////////////////////////////////////////////////////////////////////*/
 
-// began work at 1005, took break 1125 to 1153 to cook and eat breakfast
-// took another break at 1300
+// began work at 1005, took break 1125 to 1153 to cook and eat breakfast (28 mins)
+// another break between 2 and 220
+// as of 230pm approximately 3.5 hours invested
 
 // main.js
 var Twitter = require('twitter');
@@ -38,12 +61,14 @@ var vcitable; // name of artist cited throughout text
 var vsubject; // subject 1, 2 and 3 of paper
 var vsubject2;
 var vsubject3;
-var prefixes = ["post","neo","sub","pre"];
+var prefixes = ["post","neo","sub","pre"/*, "proto"*/];
 var intellectuals = ["Lacan" , "Derrida" , "Baudrillard" , "Sartre" ,
 	"Foucault" , "Marx" , "Debord" , "Bataille" , "Lyotard" , "Sontag"];
 var ideologies = [ 		"capitalism" , "Marxism" , "socialism" , "feminism"
 						, "libertarianism" , "objectivism" , "rationalism" , "nationalism"
-						, "nihilism" ];
+						, "nihilism" 
+						/*, "#globalism", "#trumpism", "#accelerationism"*/
+						];
 var artMovements = [ 	"surrealism" , "modernism" , "realism" , "social realism"
 						, "socialist realism" , "constructivism" , "expressionism"
 						/*, "futurism", "retrofuturism"*/ ];
@@ -53,7 +78,7 @@ var citableArtists = [	"Burroughs" , "Joyce" , "Gibson"
 var uncitableArtists = [ "Koons" , "Mapplethorpe" , "Glass" , "Lynch" , "Fellini" , "Cage" , "McLaren" ];
 var adjectivesThree = [	"structural" , "semiotic" , "modern" , "constructive" , "semantic"
 						, "deconstructive" , "patriarchial" , "conceptual" , "material" ];
-var works = {
+/*var works = {
 "Spelling": ["Beverly Hills 90210","Melrose Place","Models,Inc."],
 "Pynchon": ["Gravity's Rainbow","Vineland","The Crying of Lot 49"],
 "Stone": ["JFK","Natural Born Killers","Heaven and Earth","Platoon"],
@@ -66,7 +91,7 @@ var works = {
 "Madonna": ["Erotica","Sex","Material Girl"],
 "Rushdie": ["Satanic Verses","Midnight's Children"],
 "Eco": ["The Name of the Rose","Foucault's Pendulum"]
-};
+};*/
 var concepts = {
 "Lacan": ["obscurity"],
 "Derrida": ["reading"],
@@ -92,52 +117,51 @@ var somethingOfTwos = ["failure" , "futility" , "collapse" , "fatal flaw"
 var bigAbstThings = ["culture" , "language" , "art" , "reality" , "truth" 
 	, "sexuality" , "narrativity" , "consciousness"];
 var adjectivesTwo = [
-	"capitalist",randomAdjectiveThree(),trimE(randomAdjectiveThree()),
+	"capitalist",randomArrayIndex(adjectivesThree),trimE(randomArrayIndex(adjectivesThree)),
 	"cultural","dialectic","textual"];
 var adjectives = [
-	randomAdjectiveTwo(),(randomModifierPrefix() + randomAdjectiveTwo())];
-var abstNounsTwo = ["sublimation", trimE(randomAdjectiveThree())+"ism"
+	randomArrayIndex(adjectivesTwo),(randomArrayIndex(prefixes) + randomArrayIndex(adjectivesTwo))];
+var abstNounsTwo = ["sublimation", trimE(randomArrayIndex(adjectivesThree))+"ism"
 	,"construction",	"appropriation",	"materialism",	"situationism"];
-var abstNouns = [randomAbstNounTwo(),"theory","discourse",
-				"narrative","de" + randomAbstNounTwo()];
-var bigThings = ["society" , "class" , randomBigAbstThing() , "sexual identity" ];
+var abstNouns = [randomArrayIndex(abstNounsTwo),"theory","discourse",
+				"narrative","de" + randomArrayIndex(abstNounsTwo)];
+var bigThings = ["society" , "class" , randomArrayIndex(bigAbstThings) , "sexual identity" ];
 
-var candidTitles = [ randomDoingSomethingTo() + " " + randomIntellectual(),
-					randomAdjective() + " " + pluralise(randomAbstNoun()),
-					"The " + randomConcreteAdjective() + " " + randomConcreteNoun(),
-					"The " + somethingOfTwo() + " of " + randomBigNebulousThing(),
-					"The " + somethingOfTwo() + " of " + randomBigThing(),
-					"The " + randomBigNebulousThing() + " of " + somethingOfTwo(),
-					pluralise(randomBigNebulousThing()) + " of " + somethingOfTwo(),
-					randomDoingSomethingToMovement() + " " + randomArtMovement()
+var candidTitles = [ randomArrayIndex(doingSomethingTos) + " " + randomArrayIndex(intellectuals),
+					randomArrayIndex(adjectives) + " " + pluralise(randomArrayIndex(abstNouns)),
+					"The " + randomArrayIndex(concreteAdjectives) + " " + randomArrayIndex(concreteNouns),
+					"The " + randomArrayIndex(somethingOfTwos) + " of " + randomArrayIndex(bigNebulousThings),
+					"The " + randomArrayIndex(somethingOfTwos) + " of " + randomArrayIndex(bigThings),
+					"The " + randomArrayIndex(bigNebulousThings) + " of " + randomArrayIndex(somethingOfTwos),
+					pluralise(randomArrayIndex(bigNebulousThings)) + " of " + randomArrayIndex(somethingOfTwos),
+					randomArrayIndex(doingSomethingToMovements) + " " + randomArrayIndex(artMovements)
 
 ];
 //var authors = [randomAuthorInst(),randomAuthorInst(),randomAuthorInst()];
 
 
-var terms = [termAboutIntellectual(randomIntellectual()),
-			randomAdjective() + " " + randomAbstNoun(),
-			randomAdjective() + " " + randomAbstNoun(), 
-			randomAdjective() + " " + randomAdjective() + " theory",
-			"the " + randomAdjective() + " paradigm of " + randomBigNebulousThing(),
-			randomAdjective() + " " + randomIdeology()];
+var terms = [termAboutIntellectual(randomArrayIndex(intellectuals)),
+			randomArrayIndex(adjectives) + " " + randomArrayIndex(abstNouns),
+			randomArrayIndex(adjectives) + " " + randomArrayIndex(abstNouns), 
+			randomArrayIndex(adjectives) + " " + randomArrayIndex(adjectives) + " theory",
+			"the " + randomArrayIndex(adjectives) + " paradigm of " + randomArrayIndex(bigNebulousThings),
+			randomArrayIndex(adjectives) + " " + randomArrayIndex(ideologies)];
 
 var titles = [
-	randomTitleTwo(), randomCandidTitle() + ": " + randomTitleTwo()];
+	randomTitleTwo(), randomCandidTitle() + ": " + capitalizeFirstLetter(randomTitleTwo())];
 
 
 function randomArrayIndex(arr){
-	//console.log(arr);
-	//try{
-	var index = Math.floor((Math.random() * arr.length));
-	//console.log(arr.length);
+	
+	try{
+		var index = Math.floor((Math.random() * arr.length));
 
-	return arr[index];
-	//} catch (err){
-	//	console.log("array: " + arr)
+		return arr[index];
+
+	} catch (err){
 	//	return "neomodernist";
-		// return randomArrayIndex(err.message.split(" "));
-	//}
+		return err.message;
+	}
 	
 
 }
@@ -157,13 +181,14 @@ function stripThe(str){
 	return str;
 }
 
+/*
 //return a random work from a given artist
 function makeCite(artist){
 	if(works.hasOwnProperty(artist))
 		return randomArrayIndex(works[artist]);
 
-	return makeCite(randomCitableArtist());
-}
+	return makeCite(randomArrayIndex(citableArtists));
+}*/
 
 //return a random concept associated with an intellectual
 function makeConcept(intellectual){
@@ -177,9 +202,11 @@ function makeConcept(intellectual){
 function pluralise(str){
 	var lastChar = str[str.length-1];
 	if(lastChar == "y")
-		return str.substring(0,str.length-1) + "ies";
+		return replaceLastLetterWith(str,lastChar,"ies")
+		//return str.substring(0,str.length-1) + "ies";
 	if(lastChar == "s")
-		return str.substring(0,str.length-1) + "es";
+		return replaceLastLetterWith(str,lastChar,"ses")
+		//return str.substring(0,str.length-1) + "es";
 
 	return str + "s";
 }
@@ -208,13 +235,13 @@ function randomTitleTwo(){
 	if(rand >= 2/3){
 		vsubject = newTerm();
 	} else if (rand >= 1/3) {
-		vsubject = randomIdeology();
+		vsubject = randomArrayIndex(ideologies);
 	} else {
-		vsubject = randomArtMovement();
+		vsubject = randomArrayIndex(artMovements);
 	}
 
 	vsubject2 = newTerm();
-	vcitable = randomCitableArtist();
+	vcitable = randomArrayIndex(citableArtists);
 
 	var titlesTwo = [
 	vsubject + " in the works of " + vcitable, 
@@ -253,6 +280,43 @@ function randomCandidTitle(){
 	return randomArrayIndex(candidTitles);
 }
 
+function randomArtist(){
+	if(Math.random() >= 0.5){
+		return randomArrayIndex(citableArtists);
+	}
+
+	return randomArrayIndex(uncitableArtists);
+}
+
+function termAboutIntellectual(intellectual){
+	return intellectual + "ist " + makeConcept(intellectual);
+}
+
+function newTerm(){
+	return randomArrayIndex(terms);
+}
+
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+// this doesnt really work right now
+function detectUndefined(str){
+	var words = str.split(" ");
+	var newStr = "";
+	for(var x=0;x<words.length;x++){
+		if(words[x] == "undefined")
+			words[x] = "'Neomodernities' ";
+
+		newStr += words[x]
+
+		if(x+1!=words.length)
+			newStr+=" ";
+	}
+	return newStr;
+}
+
+/*
 function randomConcreteAdjective(){
 	return randomArrayIndex(concreteAdjectives);
 }
@@ -268,17 +332,13 @@ function randomDoingSomethingTo(){
 function randomDoingSomethingToMovement(){
 	return randomArrayIndex(doingSomethingToMovements);
 }
-/*
+
 function formattedAuthors(){
 	return randomArrayIndex(authors);
 }
 
 function randomAuthorInst(){
 	// department + acad institution
-}*/
-
-function termAboutIntellectual(intellectual){
-	return intellectual + "ist " + makeConcept(intellectual);
 }
 
 function somethingOfTwo(){
@@ -295,10 +355,10 @@ function randomArtMovement(){
 
 
 }
-/*
+
 function adjectiviseIsm(){
 
-}*/
+}
 
 // adjectives
 // adverbs
@@ -313,14 +373,6 @@ function randomBigThing(){
 
 function randomBigAbstThing(){
 	return randomArrayIndex(bigAbstThings);
-}
-
-function randomArtist(){
-	if(Math.random() >= 0.5){
-		return randomCitableArtist();
-	}
-
-	return randomUncitableArtist();
 }
 
 function randomCitableArtist(){
@@ -369,19 +421,12 @@ function randomAbstNoun(){
 
 function randomAbstNounTwo(){
 	return randomArrayIndex(abstNounsTwo);
-}
-
-function newTerm(){
-	return randomArrayIndex(terms);
-}
-
-console.log(
-	randomTitle()
-);
+}*/
 
 
+var message = detectUndefined(capitalizeFirstLetter(randomTitle()));
 
-//var message = randomTitle() + " " + formattedAuthors();
+console.log(message);
 
 //t.post('statuses/update',{"status": message});
 /*
