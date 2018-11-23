11475ad2aa7c48b6b77ed87031bfd09850b5697f
Zac Finger <zac@Zacs-MacBook-Pro.local>
2018-11-23 14:43:51 -0700

    it is now MVP enough to be set loose in the wild at twitter.com/randomfootnote

--- test.js
+++
@@ -1,9 +0,0 @@
-var fs = require('fs');
-var path = require('path');
-var readStream = fs.createReadStream('wordtotal', 'utf8');
-let data = ''
-readStream.on('data', function(chunk) {
-    data += chunk;
-}).on('end', function() {
-    console.log(data);
-})
